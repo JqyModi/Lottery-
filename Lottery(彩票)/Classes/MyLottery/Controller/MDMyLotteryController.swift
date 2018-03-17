8d45198d420aba434413199350a6258c8251d76d
JqyModi <1294432350@qq.com>
2018-03-17 11:59:23 +0800

    我的彩票界面+登录按钮背景图片待切割

---
+++ Lottery(彩票)/Classes/MyLottery/Controller/MDMyLotteryController.swift
@@ -0,0 +1,24 @@
+//
+//  MDMyLotteryController.swift
+//  Lottery(彩票)
+//
+//  Created by mac on 2018/3/17.
+//  Copyright © 2018年 modi. All rights reserved.
+//
+
+import UIKit
+
+class MDMyLotteryController: UIViewController {
+
+    @IBOutlet weak var loginBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //切割图片解决图片模糊背景
+        setLoginBtnBg()
+    }
+
+    private func setLoginBtnBg() {
+        
+    }
+}
