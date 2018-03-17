d87d58c92dc43b55971b9499aad10232899e83df
JqyModi <1294432350@qq.com>
2018-03-17 20:29:18 +0800

    拉升显示按钮背景图片

--- Lottery(彩票)/Classes/MyLottery/Controller/MDMyLotteryController.swift
+++ Lottery(彩票)/Classes/MyLottery/Controller/MDMyLotteryController.swift
@@ -19,6 +19,16 @@ class MDMyLotteryController: UIViewController {
     }
 
     private func setLoginBtnBg() {
+        //获取背景图片
+        var bgImage = loginBtn.backgroundImage(for: .normal)
+        var bgSelImage = loginBtn.backgroundImage(for: .normal)
+        //拉升切割背景图片并设置：通过切割四个角中间拉升来解决图片拉升变形
+        bgImage = bgImage?.stretchableImage(withLeftCapWidth: Int((bgImage?.size.width)! * 0.5), topCapHeight: Int((bgImage?.size.height)! * 0.5))
+        
+//        bgImage = bgImage?.resizableImage(withCapInsets: UIEdgeInsets.init(top: 15, left: 50, bottom: 15, right: 50), resizingMode: .stretch)
+        
+        bgSelImage = bgSelImage?.stretchableImage(withLeftCapWidth: Int((bgSelImage?.size.width)! * 0.5), topCapHeight: Int((bgSelImage?.size.height)! * 0.5))
+        loginBtn.setBackgroundImage(bgImage, for: .normal)
         
     }
 }
